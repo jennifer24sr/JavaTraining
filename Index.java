a6d87e2f34c23684cdaaf3d273582e095b0179f7
sowmya <sowmyajennifer24@gmail.com>
2022-04-22 12:05:13 -0700

    Completed : Backend Assessment

---
+++ Index.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Index {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,9,3,9,2,4,9};
+        for(int j = 1; j<=3; j++){
+            for(int i = arr.length-1; i >= 1; i--){
+                if(arr[i] == 9){
+                        int temp = arr[i-1];
+                        arr[i-1] = arr[i];
+                        arr[i] = temp;
+                        System.out.println("temp :" + temp);
+                    }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
